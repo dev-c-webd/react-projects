34c8b32537d1c6afd1c0baa4460362012692a053
kalash <kalash21504@gmail.com>
2025-10-03 02:41:40 +0530

    todo app almost completed

--- Theme one/src/App.jsx
+++ Theme one/src/App.jsx
@@ -23,11 +23,11 @@ const App = () => {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                     udukttmtyg
+                     
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-                       /////
+                       
           </div>
         </div>
       </div>
